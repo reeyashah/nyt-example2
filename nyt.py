f178406293ac72416bf1aa4e218f2ab160fa5c92
Reeya Shah <reeyashah@0587343634.wireless.umich.net>
2018-02-01 10:00:06 -0500

    add

---
+++ nyt.py
@@ -0,0 +1,13 @@
+import requests
+import secrets
+
+# nyt_key = '18c964fb5e8a4195aa7282da3347988e'
+def get_stories(section):
+    baseurl = 'https://api.nytimes.com/svc/topstories/v2/'
+    extendedurl = baseurl + section + '.json'
+    params={'api-key': secrets.nyt_key}
+    return requests.get(extendedurl, params).json()
+
+section = 'science'
+stories = get_stories(section)
+print(stories) # should print a big pile of json
